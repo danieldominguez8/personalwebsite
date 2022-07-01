307bf966c38da673f7dd383655109e9c09d23eab
dandav8 <dandav8@gmail.com>
2022-06-30 22:03:43 -0500

    Biography and Picture Formatting Added Amplify added to deploy to website

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import danny from './assets/danny.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div className="Body">
+      <div className="splitScreen">
+        <div className="leftPane">
+          <img className="bioPic" src={danny} />
+        </div>
+        <div className="rightPane">
+          <h1 className="bioTitle">Hello, my name is Danny Dominguez and I'm a Software Engineer</h1>
+          <h3 className="bioH3">About Me</h3>
+          <p className="bioText">I am a passionate Software Engineer with high aspirations.
+            I am en route to earning my M.S. in Software Engineering at Drexel University by the end of the summer quarter.
+            I have studied under amazing professors and worked alongside great classmates on various projects.
+            I have learned in-depth about software design, architecture, and requirements and have had experience working with JavaScript frameworks (ReactNative and ReactJS).
+          </p>
+          <h3 className="bioH3">Hobbies</h3>
+          <p className="bioText">
+            I love learning, coding, traveling, staying active, and spending time with my family and friends.
+          </p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
