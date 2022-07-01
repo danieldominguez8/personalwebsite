69a7742ce3835ab1c5e7455b762c006d4d9a432b
dandav8 <dandav8@gmail.com>
2022-06-30 22:15:11 -0500

    Refactored CSS to match naming conventions

--- src/App.js
+++ src/App.js
@@ -4,27 +4,25 @@ import danny from './assets/danny.png';
 
 function App() {
   return (
-    <div className="Body">
-      <div className="splitScreen">
-        <div className="leftPane">
-          <img className="bioPic" src={danny} />
+    <div className="body-container">
+      <div className="biography-section">
+        <div className="picture-container">
+          <img className="biography-picture" src={danny} />
         </div>
-        <div className="rightPane">
-          <h1 className="bioTitle">Hello, my name is Danny Dominguez and I'm a Software Engineer</h1>
-          <h3 className="bioH3">About Me</h3>
-          <p className="bioText">I am a passionate Software Engineer with high aspirations.
+        <div className="about-me-container">
+          <h1 className="biography-header">Hello, my name is Danny Dominguez and I'm a Software Engineer</h1>
+          <h3 className="biography-section-header">About Me</h3>
+          <p className="about-me-text">I am a passionate Software Engineer with high aspirations.
             I am en route to earning my M.S. in Software Engineering at Drexel University by the end of the summer quarter.
             I have studied under amazing professors and worked alongside great classmates on various projects.
             I have learned in-depth about software design, architecture, and requirements and have had experience working with JavaScript frameworks (ReactNative and ReactJS).
           </p>
-          <h3 className="bioH3">Hobbies</h3>
-          <p className="bioText">
+          <h3 className="biography-section-header">Hobbies</h3>
+          <p className="about-me-text">
             I love learning, coding, traveling, staying active, and spending time with my family and friends.
           </p>
         </div>
       </div>
-
-
     </div>
   );
 }
